a09e2d6bea501a2eea0cfa103d0fef759478fc7c
Programmer2011bird <maysamshaker7@gmail.com>
2025-02-21 00:55:57 +0330

    documented the code and deleted the unneccessary ListToTensor class

--- dataset.py
+++ dataset.py
@@ -1,5 +1,5 @@
 from torchtext.vocab import build_vocab_from_iterator, vocab
-from torchtext.transforms import VocabTransform
+from torchtext.transforms import ToTensor, VocabTransform
 from torchtext.data.utils import get_tokenizer
 from torch.utils.data import DataLoader, Dataset
 from torch.nn.functional import pad
@@ -9,13 +9,14 @@ import re
 
 
 def clean_text(text: str) -> str:
-    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text) # filtering all the shit we dont need
     return text.lower()
 
 def process_raw_txt(IN_file: str, OUT_file: str) -> None:
     file_path: str = IN_file
-    df: pd.DataFrame = pd.read_csv(file_path, sep="\t", header=None, names=["label", "text"])
-    df["text"].apply(clean_text)
+    df: pd.DataFrame = pd.read_csv(file_path, sep="\t", header=None, names=["label", "text"]) # sep="/t" replaces labels and texts that are seperated by a tab
+    df["text"].apply(clean_text) # applying the filter
+
     with open(OUT_file, "w+") as file:
         file.write(df.to_csv(index=False))
 
@@ -29,17 +30,9 @@ class SMSSpamDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, index):
-        text = self.df.iloc[index]["text"]
-        label = self.df.iloc[index]["label"]
+        text = self.df.iloc[index]["text"] # getting the text of a specific index
+        label = self.df.iloc[index]["label"] # getting the label of a specific index
         return self.tokenizer(text), label
-
-class ListToTensor(torch.nn.Module):
-    def __init__(self, dtype=torch.long):
-        super().__init__()
-        self.dtype = dtype
-
-    def forward(self, x):
-        return torch.tensor(x, dtype=self.dtype)
 
 class TruncateAndPad(torch.nn.Module):
     def __init__(self, max_length, pad_value):
@@ -51,7 +44,7 @@ class TruncateAndPad(torch.nn.Module):
         if x.size(0) > self.max_length:
             x = x[:self.max_length]  # Truncate
         else:
-            x = pad(x, (0, self.max_length - x.size(0)), value=self.pad_value)
+            x = pad(x, (0, self.max_length - x.size(0)), value=self.pad_value) # extra cushion for the comfort of our values :3
         
         return x
 
@@ -61,17 +54,18 @@ def yield_tokens(data_iter):
     for tokens, _ in data_iter:
         yield tokens
 
-vocab = build_vocab_from_iterator(yield_tokens(raw_dataset), min_freq=2, specials=["<unk>"])
-vocab.set_default_index(vocab["<unk>"])
+vocab = build_vocab_from_iterator(yield_tokens(raw_dataset), min_freq=2, specials=["<unk>"]) # building the vocabulary
+vocab.set_default_index(vocab["<unk>"]) # setting the default to unknown tensor
 
 max_sentence_length = 50
 text_transform = torch.nn.Sequential(
     VocabTransform(vocab=vocab),
-    ListToTensor(),  # Use the custom module
+    ToTensor(),
     TruncateAndPad(max_sentence_length, vocab["<unk>"])
-)
+) # Transformer
 
 def collate_batch(batch):
+    # Handles batches
     texts, labels = [], []
 
     for tokens, label in batch:
@@ -79,7 +73,7 @@ def collate_batch(batch):
         label = 0 if label == "ham" else 1
         labels.append(label)
 
-    return torch.stack(texts), torch.tensor(labels, dtype=torch.long)
+    return torch.stack(texts), torch.tensor(labels, dtype=torch.long) # torch.stack because the texts list is a "stack" of tensors in a list
 
 def dataset(BATCH_SIZE: int = 32):
     dataloader = DataLoader(raw_dataset, BATCH_SIZE, collate_fn=collate_batch)
